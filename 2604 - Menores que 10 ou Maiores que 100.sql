ab913bfe7c789bcb7e0dfbb342f01c1b3bd431e0
Humberto Mendes <humberto.hms36@gmail.com>
2024-08-23 21:46:12 -0300

    SQL

---
+++ 2604 - Menores que 10 ou Maiores que 100.sql
@@ -0,0 +1,9 @@
+/*O setor financeiro da empresa precisa de um relatório que mostre o código e o nome dos produtos cujo preço são menores que 10 ou maiores que 100.*/
+
+SELECT
+    id,
+    name
+FROM
+    products
+WHERE
+    price <10 or price >100
